bf04aa516cb8e1240acb79f55c3f29cbb417da4c
Mateo Bedoya <mateo.bedoyao@udea.edu.co>
2023-11-09 16:35:06 -0500

    eventos en espera

--- pages/eventosEnEspera.tsx
+++ pages/eventosEnEspera.tsx
@@ -3,7 +3,7 @@ export default function Home() {
         <div className="w-full h-screen bg-gradient-to-r to-blue-800 from-white flex items-center justify-center">
             <div className="bg-white rounded-lg p-8 shadow-md w-96">
                 <h1 className="text-3xl font-bold text-center mb-4">
-                   Eventos es espera
+                   Eventos es Espera
                 </h1>
             </div>
         </div>
